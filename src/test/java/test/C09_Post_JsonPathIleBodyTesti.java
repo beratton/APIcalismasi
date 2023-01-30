8df85126e9596515b7e3508d94a783b34c995661
beratton <beratton@protonmail.com>
2023-01-30 23:25:28 +0300

    30ocak23

--- src/test/java/test/C09_Post_JsonPathIleBodyTesti.java
+++ src/test/java/test/C09_Post_JsonPathIleBodyTesti.java
@@ -1,5 +1,14 @@
 package test;
 
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
 public class C09_Post_JsonPathIleBodyTesti {
     /*
             https://restful-booker.herokuapp.com/booking
@@ -29,5 +38,52 @@ public class C09_Post_JsonPathIleBodyTesti {
                 ve "additionalneeds“in,"wi-fi"
             oldugunu test edin
      */
+
+@Test
+    public void post01(){
+    // 1- Request URL ve Body hazirlama
+    String url="https://restful-booker.herokuapp.com/booking";
+/* once inner body olusturulur. sonra distaki
+    "bookingdates" : {
+        "checkin" : "2021-06-01",
+                "checkout" : "2021-06-10"
+    },
+*/
+    JSONObject innerBody= new JSONObject();
+    innerBody.put("checkin" , "2021-06-01");
+    innerBody.put("checkout" , "2021-06-10");
+
+    JSONObject reqBody= new JSONObject();
+    reqBody.put("firstname","Ali");
+    reqBody.put("lastname","Bak");
+    reqBody.put("totalprice",500);
+    reqBody.put("depositpaid",false);
+    reqBody.put("bookingdates" , innerBody);
+    reqBody.put("additionalneeds","wi-fi");
+    // 2- Expected Data hazirla
+
+    //3 - Response u kaydet
+    Response response = given().contentType(ContentType.JSON).
+            when().
+            body(reqBody.toString()).
+            post(url);
+
+    //response.prettyPrint();
+    // 4 - Assertion
+
+    response.
+            then().
+            assertThat().
+            statusCode(200).
+            contentType(ContentType.JSON).
+            body("booking.firstname", equalTo("Ali"),
+                    "booking.lastname",equalTo("Bak"),
+                    "booking.totalprice",equalTo(500),
+                    "booking.depositpaid",equalTo(false),
+                    "booking.bookingdates.checkin",equalTo("2021-06-01"),
+                    "booking.bookingdates.checkout",equalTo("2021-06-10"),
+                    "booking.additionalneeds","wi-fi");
+
+}
 
 }
